0feb02502eb05c2d2d3901b4563761ae14dd3ebf
Чесников Максим <maxim.chesnikov@chulakov.ru>
2021-03-23 11:06:07 +0300

    Initial Commit

---
+++ GithubTestSwuftUIApp/GithubTestSwuftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubTestSwuftUIAppApp.swift
+//  GithubTestSwuftUIApp
+//
+//  Created by Студия on 23.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct GithubTestSwuftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
